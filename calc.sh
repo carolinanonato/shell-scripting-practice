945cc8064241b189d03065d8e53a8a3d78920f2d
Carolina Nonato <csnonato@gmail.comgit config --global user.email>
2021-11-07 10:08:56 -0400

    Displayed the operations and confirmed.

--- calc.sh
+++ calc.sh
@@ -1,3 +1,9 @@
-echo "Please write your name: "
+echo "Please write your name:"
 read name
-echo "What a nice surprise $name, welcome!"
+echo "What a nice surprise $name, welcome to the Calculator!"
+echo "-------"
+echo "First number, please:"
+read num1 
+echo "Second number:"
+read num2
+echo "Alright, you choose" $num1 "and" $num2"."

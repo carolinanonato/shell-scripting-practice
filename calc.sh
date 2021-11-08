cbbcb44665a65c320e7edebdae51e3b12a5632fb
Carolina Nonato <csnonato@gmail.comgit config --global user.email>
2021-11-08 09:24:46 -0500

    Inserted the addition operation and tested for uppercase and lowercase

--- calc.sh
+++ calc.sh
@@ -1,15 +1,23 @@
+
 echo "Please write your name:"
 read name
 echo "What a nice surprise $name, welcome to the Calculator!"
-echo "-------"
+echo "♥§◄-------►§♥"
 echo "First number, please:"
 read num1 
 echo "Second number:"
 read num2
 echo "Alright, you choose" $num1 "and" $num2"."
 echo "Please choose the operation you would like to complete: 
-A for Addition
-S for Subtraction
-M for Multiplication
-D for Division"
+a for Addition
+s for Subtraction
+m for Multiplication
+d for Division"
 read operation
+
+case $operation in
+	"A"|"a")
+	echo "The sum is: "
+	expr $num1 + $num2
+;;
+esac

f4b1902951d710f25ac22cecdf9da6f87e45d95a
Carolina Nonato <csnonato@gmail.comgit config --global user.email>
2021-11-08 10:16:16 -0500

    While loop + refactoring to work well

--- calc.sh
+++ calc.sh
@@ -1,44 +1,57 @@
-\
 echo "Please write your name:"
 read name
 echo "What a nice surprise $name, welcome to the Calculator!"
 echo "--------------------"
-echo "First number, please:"
-read num1 
-echo "Second number:"
-read num2
-echo "Alright, you choose" $num1 "and" $num2"."
-echo "Now, please choose the operation you would like to complete: 
-a for Addition
-s for Subtraction
-m for Multiplication
-d for Division
-e for Exit"
-read operation
+while true
+do
+	echo "Please choose the operation you would like to complete: 
+	a for Addition
+	s for Subtraction
+	m for Multiplication
+	d for Division
+	e for Exit"
+	read operation
 
-case $operation in
-	"A"|"a")
-	echo $num1 "+" $num2 "="
-	expr $num1 + $num2
-	;;
-	"S"|"s")
-	echo $num1 "-" $num2 "="
-	expr $num1 - $num2
-	;;
-	"M"|"m")
-	echo $num1 "x" $num2 "="
-	expr $num1 "*" $num2
-	;;
-	"D"|"d")
-	echo $num1 "/" $num2 "="
-	expr $num1  / $num2
-	;;
-	"E"|"e")
-	echo "No problem, bye $name!"
-	;;
-	*)
-	echo "Sorry, that's not one of my functions. Can you try again?"
-	;;
-	
-esac
-
+	case $operation in
+		"A"|"a")
+		echo "First number:"
+		read num1 
+		echo "Second number:"
+		read num2
+		echo $num1 "+" $num2 "="
+		expr $num1 + $num2
+		;;
+		"S"|"s")
+		echo "First number:"
+		read num1 
+		echo "Second number:"
+		read num2
+		echo $num1 "-" $num2 "="
+		expr $num1 - $num2
+		;;
+		"M"|"m")
+		echo "First number:"
+		read num1 
+		echo "Second number:"
+		read num2
+		echo $num1 "x" $num2 "="
+		expr $num1 "*" $num2
+		;;
+		"D"|"d")
+		echo "First number:"
+		read num1 
+		echo "Second number:"
+		read num2
+		echo $num1 "/" $num2 "="
+		expr $num1  / $num2
+		;;
+		"E"|"e")
+		echo "No problem, bye $name!"
+		exit
+		;;
+		*)
+		echo "Sorry, that's not one of my functions. Can you try again?"
+		;;
+		
+	esac
+done

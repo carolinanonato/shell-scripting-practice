5f5bfd1b1af7ebf46be9bb10be10f643bf4de5d7
Carolina Nonato <csnonato@gmail.comgit config --global user.email>
2021-11-08 09:45:35 -0500

    All the operations + Exit are done

--- calc.sh
+++ calc.sh
@@ -1,23 +1,40 @@
-
+\
 echo "Please write your name:"
 read name
 echo "What a nice surprise $name, welcome to the Calculator!"
-echo "♥§◄-------►§♥"
+echo "--------------------"
 echo "First number, please:"
 read num1 
 echo "Second number:"
 read num2
 echo "Alright, you choose" $num1 "and" $num2"."
-echo "Please choose the operation you would like to complete: 
+echo "Now, please choose the operation you would like to complete: 
 a for Addition
 s for Subtraction
 m for Multiplication
-d for Division"
+d for Division
+e for Exit"
 read operation
 
 case $operation in
 	"A"|"a")
-	echo "The sum is: "
+	echo $num1 "+" $num2 "="
 	expr $num1 + $num2
-;;
+	;;
+	"S"|"s")
+	echo $num1 "-" $num2 "="
+	expr $num1 - $num2
+	;;
+	"M"|"m")
+	echo $num1 "x" $num2 "="
+	expr $num1 "*" $num2
+	;;
+	"D"|"d")
+	echo $num1 "/" $num2 "="
+	expr $num1  / $num2
+	;;
+	"E"|"e")
+	echo "No problem, bye $name!"
+	;;
+	
 esac
